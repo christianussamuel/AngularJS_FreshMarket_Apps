dc0855d71b198e88518cdaf946c5ce63b058f49e
christianussamuel <39821996+christianussamuel@users.noreply.github.com>
2020-05-08 18:54:13 +0700

    ModernWeb Last Project

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,50 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {ProductComponent} from './components/product/product.component';
+import {CartComponent} from './components/cart/cart.component';
+import {CheckoutComponent} from './components/checkout/checkout.component';
+import {ThankyouComponent} from './components/thankyou/thankyou.component';
+import {LoginComponent} from './components/login/login.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {ProfileGuard} from './guard/profile.guard';
+import {RegisterComponent} from './components/register/register.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'product/:id', component: ProductComponent
+  },
+  {
+    path: 'cart', component: CartComponent
+  },
+  {
+    path: 'checkout', component: CheckoutComponent, canActivate: [ProfileGuard]
+  },
+  {
+    path: 'thankyou', component: ThankyouComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'profile', component: ProfileComponent, canActivate:[ProfileGuard]
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: '**', pathMatch: 'full', redirectTo: ''
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,70 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {CartComponent} from './components/cart/cart.component';
+import {CheckoutComponent} from './components/checkout/checkout.component';
+import {HomeComponent} from './components/home/home.component';
+import {ProductComponent} from './components/product/product.component';
+import {ThankyouComponent} from './components/thankyou/thankyou.component';
+import {HttpClientModule} from '@angular/common/http';
+import {NgxSpinnerModule} from 'ngx-spinner';
+import {ToastrModule} from 'ngx-toastr';
+import {EmailValidator, FormsModule, NG_ASYNC_VALIDATORS, ReactiveFormsModule} from '@angular/forms';
+import {LoginComponent} from './components/login/login.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {AuthServiceConfig, GoogleLoginProvider, SocialLoginModule} from 'angularx-social-login';
+import {RegisterComponent} from './components/register/register.component';
+
+const config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider('932414536881-tli678nuo6hdkdhdqvm7ssopjr2vfban.apps.googleusercontent.com')
+  }
+
+]);
+
+export function provideConfig() {
+  return config;
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    CheckoutComponent,
+    HomeComponent,
+    ProductComponent,
+    ThankyouComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+    ToastrModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
+    SocialLoginModule
+  ],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
